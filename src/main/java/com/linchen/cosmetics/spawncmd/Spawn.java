d19f9c790cf2e8ff9a343f5a676a0c309a45152f
Linchen <lealeyd22@gmail.com>
2021-04-30 21:31:59 +0200

    Initial commit

---
+++ src/main/java/com/linchen/cosmetics/spawncmd/Spawn.java
@@ -0,0 +1,20 @@
+package com.linchen.cosmetics.spawncmd;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Spawn implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            //
+
+        }
+        return false;
+    }
+}
